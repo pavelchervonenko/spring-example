af4861a86e3278b39cf19d44578572f49a16c81b
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-12-12 22:45:00 +0300

    add configuration

---
+++ src/main/java/io/hexlet/spring/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package io.hexlet.spring.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @Value("${app.welcome-message}")
+    private String welcomeMessage;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return welcomeMessage;
+    }
+}
